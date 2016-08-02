4be1e5280b132c244a9626c9f37d4de88a6a04e7
CloudNiner <andrew@cloudniner.co>
2016-08-02 13:10:38 -0400

    Update server ports to non-conflicting values

--- conf/browsersync-dist.conf.js
+++ conf/browsersync-dist.conf.js
@@ -2,6 +2,10 @@ const conf = require('./gulp.conf');
 
 module.exports = function () {
   return {
+    port: 7880,
+    ui: {
+      port: 7881
+    },
     server: {
       baseDir: [
         conf.paths.dist
--- conf/browsersync.conf.js
+++ conf/browsersync.conf.js
@@ -2,6 +2,10 @@ const conf = require('./gulp.conf');
 
 module.exports = function () {
   return {
+    port: 7878,
+    ui: {
+      port: 7879
+    },
     server: {
       baseDir: [
         conf.paths.tmp,
