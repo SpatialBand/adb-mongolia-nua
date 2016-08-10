10e1a0f74e2405b903c640c18efa7a056407502e
CloudNiner <andrew@cloudniner.co>
2016-08-10 14:15:27 -0400

    Add selected soum to data view map
    Style via css so designers can easily modify

--- src/app/views/data/data.js
+++ src/app/views/data/data.js
@@ -1,5 +1,8 @@
 import {ADBMapController} from '../../adb-map.controller';
 
+const L = require('leaflet');
+const cartodb = require('cartodb');
+
 class DataViewController extends ADBMapController {
 
   /** @ngInject */
@@ -14,11 +17,27 @@ class DataViewController extends ADBMapController {
 
     this.soumCode = this.$stateParams.soumCode;
     this.$log.debug('soum code:', this.soumCode);
+
+    this._getSoumForMap();
   }
 
   _setupMap(options) {
     options.options.scrollWheelZoom = false;
     super._setupMap(options);
+  }
+
+  // TODO: Use whatever sql service @rmartz creates for that task
+  _getSoumForMap() {
+    const sql = new cartodb.SQL({user: this.config.carto.accountName});
+    sql.execute("SELECT the_geom FROM soums WHERE soumcode = {{soumcode}}", {
+      soumcode: this.soumCode
+    }, {
+      format: 'geojson'
+    }).done(data => this._addGeoJSONLayer(data));
+  }
+
+  _addGeoJSONLayer(geojson) {
+    L.geoJson(geojson).addTo(this.map);
   }
 }
 
