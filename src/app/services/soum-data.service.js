e3e76d899112ff506ad48f787450e97f642a0cf5
Reed Martz <rmartz@azavea.com>
2016-08-10 15:40:35 -0400

    Convert to for...of for cleaner code

--- src/app/services/soum-data.service.js
+++ src/app/services/soum-data.service.js
@@ -1,22 +1,15 @@
 /* ngInject */
 export function SoumData($log, $http, $q, Config) {
-  const username = 'adbmongolia';
-
   return {
     load: soumId => {
       // Return a promise that resolves with the constructed soum
-      return $q((resolve, reject) => {
-        // Send a HTTP request to load the soum's raw data
-        loadSoumData(soumId).then(responses => {
-          // Pass all response objects to merge them together as one row
-          const combinedRow = mergeResponses(responses);
-          // Format the combined result with the same structure as Config
-          const soum = formatSoumData(combinedRow);
-          // Done!
-          resolve(soum);
-        }, () => {
-          reject();
-        });
+      return loadSoumData(soumId).then(responses => {
+        // Pass all response objects to merge them together as one row
+        const combinedRow = mergeResponses(responses);
+        // Format the combined result with the same structure as Config
+        const soum = formatSoumData(combinedRow);
+        // Done!
+        return soum;
       });
     }
   };
@@ -26,25 +19,22 @@ export function SoumData($log, $http, $q, Config) {
     //  an object with the combined columns from each. Repeated column names
     //  are overwritten.
     const result = {};
-    for (let i = 0; i < responses.length; i++) {
-      const row = responses[i].data.rows[0];
-      for (const key in row) {
-        if (row.hasOwnProperty(key)) {
-          result[key] = row[key];
-        }
+    for (const response of responses) {
+      const row = response.data.rows[0];
+      for (const key of Object.keys(row)) {
+        result[key] = row[key];
       }
     }
+    $log.log(result);
     return result;
   }
 
   function formatSoumData(soumRow) {
     // Take a key->value row from CartoSQL and format it the same way as Config
     const soum = {};
-    for (const label in Config.mapSections) {
-      if (Config.mapSections.hasOwnProperty(label)) {
-        const section = Config.mapSections[label];
-        soum[label] = processSection(soumRow, section);
-      }
+    for (const label of Object.keys(Config.mapSections)) {
+      const section = Config.mapSections[label];
+      soum[label] = processSection(soumRow, section);
     }
 
     return soum;
@@ -54,9 +44,7 @@ export function SoumData($log, $http, $q, Config) {
     // Process through a section of the Config definitions and add values from
     //  CartoSQL to each field.
     const results = {};
-    for (let i = 0; i < section.visualizations.length; i++) {
-      // Create a copy of this column so we don't change the actual config
-      const variable = Object.assign({}, section.visualizations[i]);
+    for (const variable of section.visualizations) {
       // Attach this Soum's value to the column object
       variable.value = soumRow[variable.field];
       results[variable.field] = variable;
@@ -68,16 +56,14 @@ export function SoumData($log, $http, $q, Config) {
     // Parse through the config to build a mapping of {table: [field...]} for all
     //  columns referenced.
     const fields = {};
-    for (const label in Config.mapSections) {
-      if (Config.mapSections.hasOwnProperty(label)) {
-        const section = Config.mapSections[label].visualizations;
-        for (let i = 0; i < section.length; i++) {
-          const table = section[i].table || 'soums';
-          if (!fields[table]) {
-            fields[table] = [];
-          }
-          fields[table].push(section[i].field);
+    for (const label of Object.keys(Config.mapSections)) {
+      const section = Config.mapSections[label];
+      for (const variable of section.visualizations) {
+        const table = variable.table || 'soums';
+        if (!fields[table]) {
+          fields[table] = [];
         }
+        fields[table].push(variable.field);
       }
     }
     return fields;
@@ -89,18 +75,16 @@ export function SoumData($log, $http, $q, Config) {
     const fieldList = getAllFields();
     const promises = [];
 
-    for (const table in fieldList) {
-      if (fieldList.hasOwnProperty(table)) {
-        const fields = fieldList[table].join(',');
-        const query = `SELECT ${fields} FROM ${table} WHERE soumcode = ${soumId}`;
+    for (const table of Object.keys(fieldList)) {
+      const fields = fieldList[table];
+      const query = `SELECT ${fields} FROM ${table} WHERE soumcode = ${soumId}`;
 
-        const request = {
-          method: 'GET',
-          url: `https://${username}.carto.com/api/v2/sql?q=${query}`
-        };
+      const request = {
+        method: 'GET',
+        url: `https://${Config.carto.accountName}.carto.com/api/v2/sql?q=${query}`
+      };
 
-        promises.push($http(request));
-      }
+      promises.push($http(request));
     }
 
     return $q.all(promises);
