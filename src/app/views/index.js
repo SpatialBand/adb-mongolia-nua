52c479fc35514c404b0ef271c1c284e837a83ec6
Reed Martz <rmartz@azavea.com>
2016-08-29 11:52:54 -0400

    Add numeric formatting to values in the Data page
    Introduces a new filter to handle rendering numeric values in the data
    page appropriately. This filter takes in whether it should be shown as a
    percent or a change and appends the appropriate symbol to the value.

--- src/app/views/index.js
+++ src/app/views/index.js
@@ -8,11 +8,12 @@ import {adbInfoblockView} from './infoblock/infoblock';
 import {appConfig} from '../../config';
 import {adbLangModule} from '../i18n/index';
 import {adbServicesModule} from '../services/index';
+import {adbFiltersModule} from '../filters/index';
 
 export const adbViewModule = 'adb.views';
 
 angular
-  .module(adbViewModule, [appConfig, adbLangModule, adbServicesModule])
+  .module(adbViewModule, [appConfig, adbLangModule, adbServicesModule, adbFiltersModule])
   .component('adbNavbarView', adbNavbarView)
   .component('adbHomeView', adbHomeView)
   .component('adbMapView', adbMapView)
