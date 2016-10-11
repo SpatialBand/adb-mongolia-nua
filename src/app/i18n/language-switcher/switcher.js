38fc1bb515a1f63ad1a5c5e0b5f516644899b446
Reed Martz <rmartz@azavea.com>
2016-10-11 14:24:58 -0400

    Fix language change not affecting Leaflet components

--- src/app/i18n/language-switcher/switcher.js
+++ src/app/i18n/language-switcher/switcher.js
@@ -1,20 +1,32 @@
 
 class LanguageSwitcherController {
   /** @ngInject */
-  constructor($document, $translate) {
+  constructor($document, $translate, $window, $state) {
     this.$translate = $translate;
     this.$document = $document;
+    this.$window = $window;
+    this.$state = $state;
   }
 
   onSwitcherClicked() {
-    const currentLang = this.$translate.use();
+    const currentLang = this.$window.localStorage.language || 'en';
     const newLang = currentLang === 'en' ? 'mn' : 'en';
+
     if (newLang === 'mn') {
       this.$document.find('html').addClass('cyrillic');
     } else {
       this.$document.find('html').removeClass('cyrillic');
     }
-    this.$translate.use(newLang);
+
+    try {
+      // Update the local storage language so it persists across sessions
+      this.$window.localStorage.language = newLang;
+      this.$translate.use(newLang);
+      this.$state.reload();
+    } catch (e) {
+      // Local storage is disabled or unsupported, just update the translations
+      this.$translate.use(newLang);
+    }
   }
 }
 
